140beabddc6b5e3040c84b114e2dec8b50ec96c7
joao vitor ramalho lopes <joaovitorramalho@eaporta.org>
2023-11-28 18:47:41 -0300

    primeira execuçao do servidor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const express = require("espress")
+const express = require("express")
 
 const app = express()
+
+app.get('/', (requisicao, resposta) =>{
+    resposta.send("ola")
+})
+
+app.listen(3000,() => {
+    console.log("servidor rodando na porta 3000")
+})
